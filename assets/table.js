1e8b9acddf086028120f6a0c0569932baf1bece9
Stephen Holiday <stephen.holiday@gmail.com>
2022-10-22 08:07:12 -0700

    Allow pausing auto-refresh in the web-ui.

--- assets/table.js
+++ assets/table.js
@@ -1,60 +1,73 @@
-$(document).ready(function() {
-  $.getJSON("/table-dev", function(data) {
+$(document).ready(function () {
+  $.getJSON("/table-dev", function (data) {
     var items = [
-      {data : 'mac'}, 
-      {data: 'manuf'},
-      {data: 'idtype'}, {data : 'name', name : 'Device'}, {data : 'disc'},
-      {data : 'closest'}
+      { data: "mac" },
+      { data: "manuf" },
+      { data: "idtype" },
+      { data: "name", name: "Device" },
+      { data: "disc" },
+      { data: "closest" },
     ];
 
     for (i = 0; i < data.rooms.length; i++) {
-      items.push({data : data.rooms[i]});
+      items.push({ data: data.rooms[i] });
     }
 
     for (i = 0; i < items.length; i++) {
-      $("#col-head").append('<th>' + items[i].data + '</th>');
-      $("#col-footer").append('<th>' + items[i].data + '</th>');
+      $("#col-head").append("<th>" + items[i].data + "</th>");
+      $("#col-footer").append("<th>" + items[i].data + "</th>");
     }
 
-    var table = $('#example').DataTable({
-      scrollY : '500px',
-      scrollCollapse : true,
-      paging : false,
-      ajax : {
-        "url" : "/table-dev",
+    var table = $("#example").DataTable({
+      scrollY: "500px",
+      scrollCollapse: true,
+      paging: false,
+      ajax: {
+        url: "/table-dev",
       },
-      columns : items,
+      columns: items,
     });
-    setInterval(function() {
-      table.ajax.reload(null, false); // user paging is not reset on reload
+    setInterval(function () {
+      if ($("#auto-refresh-chk").is(":checked")) {
+        table.ajax.reload(null, false); // user paging is not reset on reload
+      }
     }, 500);
   });
 });
 
-$(document).ready(function() {
-  $.getJSON("/table-rooms", function(data) {
+$(document).ready(function () {
+  $.getJSON("/table-rooms", function (data) {
     var items = [
-      {data : 'name'}, {data : 'IP'}, {data : 'Uptime'}, {data : 'Firm'},
-      {data : 'Ver'}, {data : 'Rssi'}, {data : 'Adverts'}, {data : 'Seen'},
-      {data : 'Queried'}, {data : 'Reported'}
+      { data: "name" },
+      { data: "IP" },
+      { data: "Uptime" },
+      { data: "Firm" },
+      { data: "Ver" },
+      { data: "Rssi" },
+      { data: "Adverts" },
+      { data: "Seen" },
+      { data: "Queried" },
+      { data: "Reported" },
     ];
 
     for (i = 0; i < items.length; i++) {
-      $("#rcol-head").append('<th>' + items[i].data + '</th>');
-      $("#rcol-footer").append('<th>' + items[i].data + '</th>');
+      $("#rcol-head").append("<th>" + items[i].data + "</th>");
+      $("#rcol-footer").append("<th>" + items[i].data + "</th>");
     }
 
-    var table1 = $('#rooms').DataTable({
-      scrollY : '500px',
-      scrollCollapse : true,
-      paging : false,
-      ajax : {
-        "url" : "/table-rooms",
+    var table1 = $("#rooms").DataTable({
+      scrollY: "500px",
+      scrollCollapse: true,
+      paging: false,
+      ajax: {
+        url: "/table-rooms",
       },
-      columns : items,
+      columns: items,
     });
-    setInterval(function() {
-      table1.ajax.reload(null, false); // user paging is not reset on reload
+    setInterval(function () {
+      if ($("#auto-refresh-chk").is(":checked")) {
+        table1.ajax.reload(null, false); // user paging is not reset on reload
+      }
     }, 2000);
   });
 });
