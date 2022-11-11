d738ea510eb8f96616ebf5eca95294a874f62302
Stephen Holiday <stephen.holiday@gmail.com>
2022-11-11 05:46:38 -0800

    TestHandleRoomDevice

--- webapp_test.go
+++ webapp_test.go
@@ -1,15 +1,20 @@
 package eadmin
 
 import (
+	"log"
+	"os"
 	"testing"
 
+	"github.com/GPORTALcloud/ouidb/pkg/ouidb"
 	"github.com/benbjohnson/clock"
 	"github.com/stretchr/testify/assert"
 )
 
 func newTestWebApp(t *testing.T) *WebApp {
+	db, _ := ouidb.New()
 	return &WebApp{
 		clock:        clock.NewMock(),
+		ouidb:        db,
 		deviceByName: map[string]*Device{},
 		rooms:        map[string]*Room{},
 		manufByMac:   map[string]string{},
@@ -39,15 +44,38 @@ func TestGetOrInsertRoom(t *testing.T) {
 
 func TestHandleRoomTelem(t *testing.T) {
 	wa := newTestWebApp(t)
-
 	m := MockMessage{
+		_Topic:   "espresense/rooms/room_name/telemetry",
 		_Payload: []byte(`{"queried": 2}`),
 	}
-
-	wa.mqttHandlerRoomTelem(nil, &m, "room_name")
+	wa.mqttHandlerRoom(nil, &m)
 
 	val, ok := wa.rooms["room_name"]
 	assert.True(t, ok)
 	assert.Equal(t, val.Telemetry.Queried, 2)
 	assert.Equal(t, val.Telemetry.Recieved, wa.clock.Now())
+}
+
+const testMac = "000181dead01"
+
+func TestHandleRoomDevice(t *testing.T) {
+
+	wa := newTestWebApp(t)
+	m := MockMessage{
+		_Topic:   "espresense/rooms/room_name",
+		_Payload: []byte(`{"mac": "000181dead01"}`),
+	}
+	wa.mqttHandlerRoom(nil, &m)
+
+	room, ok := wa.rooms["room_name"]
+	assert.True(t, ok)
+	assert.Equal(t, room.LastPing, wa.clock.Now())
+	assert.Equal(t, room.pingsByMac[testMac].Recieved, wa.clock.Now())
+
+	assert.Equal(t, wa.manufByMac[testMac], "NortelNe")
+}
+
+func TestMain(m *testing.M) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	os.Exit(m.Run())
 }
